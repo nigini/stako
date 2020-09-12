6bce37a75dd844099c1c46a96e5ac1b61f4d79fe
Nigini Oliveira <nigini@gmail.com>
2020-09-11 19:29:04 -0700

    Fixed a bug with loading/creating a STAKO user.

--- stako-browser/chrome/oauth.js
+++ stako-browser/chrome/oauth.js
@@ -1,11 +1,10 @@
 window.onload = function () {
     chrome.identity.getAuthToken({interactive: true}, function (token) {
         if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError);
+            console.log('LOG IN FAIL: ' + chrome.runtime.lastError.message);
         } else {
-            console.log(token);
             chrome.identity.getProfileUserInfo(function (info) {
-                console.log(info.email)
+                console.log('LOGGED IN AS ' + info.email);
                 chrome.storage.local.set({email: info.email});
                 document.getElementById('login').innerText = info.email;
                 matchStakoUser(info.email);
@@ -21,12 +20,18 @@ function matchStakoUser(userEmail) {
     let user;
     chrome.storage.local.get({'STAKO_USER': null}, function (items) {
         user = items['STAKO_USER'];
-        //TODO: When should this be updated?
+        //TODO: When should this be updated? ADD some user.REFRESH?
         if (!user) {
-            user = searchStakoUser(userEmail);
-            if(!user) {
-                createStakoUser(userEmail);
-            }
+            searchStakoUser(userEmail).then( foundUser => {
+                if(!foundUser) {
+                    createStakoUser(userEmail).then( createdUser => {
+                        user = createdUser;
+                    })
+                } else {
+                    chrome.storage.local.set({STAKO_USER: foundUser});
+                    user = foundUser;
+                }
+            });
         }
         if(user) {
             document.getElementById('nickname').innerText = user.nickname;
@@ -39,13 +44,14 @@ function searchStakoUser(userEmail) {
     console.log('searching stako user: ' + userEmail);
     const search_url = STAKO_USER_URL + '?key=email&value=' + userEmail;
     const request = new Request(search_url, {method: 'GET'});
-    fetch(request)
+    return fetch(request)
         .then(response => {
+            console.debug(response);
             if (response.status === 200) {
-                response.json()
+                return response.json()
                     .then( stakoUser => {
                         console.log('found stako user: ' + JSON.stringify(stakoUser));
-                        chrome.storage.local.set({STAKO_USER: stakoUser});
+                        return stakoUser;
                     });
             } else if (response.status === 404) {
                 return null;
@@ -56,9 +62,6 @@ function searchStakoUser(userEmail) {
                     });
             }
         })
-        .then(response => {
-            console.debug(response);
-        })
         .catch(error => {
             console.error(error);
         });
@@ -67,18 +70,24 @@ function searchStakoUser(userEmail) {
 function createStakoUser(userEmail) {
     console.log('creating stako user: ' + userEmail);
     const request = new Request(STAKO_USER_URL, {method: 'POST', headers: {'Content-Type': 'application/json'}});
-    fetch(request)
+    return fetch(request)
         .then(response => {
             if (response.status === 200) {
                 response.json()
                     .then( stakoUser => {
                         console.log('created stako user: ' + JSON.stringify(stakoUser));
                         stakoUser.email = userEmail;
-                        return updateStakoUser(stakoUser);
+                        updateStakoUser(stakoUser)
+                            .then( updated_user => {
+                                return updated_user;
+                            }
+                        );
                     });
             } else {
                 response.text()
-                    .then(text => {throw Error('Could not create new STAKO user: HTTP_STATUS ' + response.status + ' + ' + text)});
+                    .then(text => {
+                        throw Error('Could not create new STAKO user: HTTP_STATUS ' + response.status + ' + ' + text)
+                    });
             }
         })
         .then(response => {
@@ -94,7 +103,7 @@ function updateStakoUser(user) {
     const request_url = STAKO_USER_URL + user.uuid + '/';
     const request = new Request(request_url,
                             {method: 'PUT', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(user)});
-    fetch(request)
+    return fetch(request)
         .then(response => {
             if (response.status === 200) {
                 chrome.storage.local.set({STAKO_USER: user});

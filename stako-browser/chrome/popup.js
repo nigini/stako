cd9093fa69073fc8ca38d0bc28722dfe47e54d71
bbrusniak <brusniak@cs.washington.edu>
2021-01-17 16:19:10 -0800

    Started adding features to popup and started implementing carosel via bootstrap

--- stako-browser/chrome/popup.js
+++ stako-browser/chrome/popup.js
@@ -10,9 +10,9 @@ function displayVisit () {
   chrome.storage.local.get(['visitList'], function(result){
       var visitList = result.visitList;
       var visit = {};
-      visit = visitList.pop();
+      //visit = visitList.pop();
       console.log(visit);
-      visitList.push(visit);
+      //visitList.push(visit);
 
       $('#url').text(visit.tabUrl);
       $('#timestamp').text(visit.timestamp);

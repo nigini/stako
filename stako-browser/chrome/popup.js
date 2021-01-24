1064cb53461e7d1bc3ec81bbe47306cf88499c35
bbrusniak <brusniak@cs.washington.edu>
2021-01-23 16:10:30 -0800

    finished up carousel with active links

--- stako-browser/chrome/popup.js
+++ stako-browser/chrome/popup.js
@@ -31,34 +31,20 @@ function addButtonFunctionality() {
 function loadCarosel() {
   chrome.storage.local.get(['STAKO_USER'], function(result) {
     var userData = result["STAKO_USER"];
-    console.log(userData);
     var nick = document.getElementById("nickname");
     var mot = document.getElementById("motto");
     var carosel = document.querySelector("#carouselContent > .carousel-inner");
-    //var tags = userData.activity["weekly_summary"][0]["top_tags"];
-    //This tags is for testing,use line 37 when getting the actual data from the API.
-    var tags = [];
-    tags.push({
-      "tag_name": "Java",
-      "pages_visits": 0
-    });
-    tags.push({
-      "tag_name": "Python",
-      "pages_visits": 0
-    });
-    tags.push({
-      "tag_name": "Git",
-      "pages_visits": 0
-    });
+    var topTags = userData["activity"]["weekly_summary"]["2021"]["3"]["top_tags"]
+    var tags = Object.keys(topTags);
     nick.textContent = userData.nickname;
     mot.textContent = userData.moto;
     for(let tag of tags) {
       var slide = document.createElement("div");
       var tagName = document.createElement("a");
       var pageVisits = document.createElement("p");
-      tagName.textContent = tag.tag_name;
-      tagName.href = "https://stackoverflow.com/tags/" + tag.tag_name;
-      pageVisits.textContent = tag.pages_visits;
+      tagName.textContent = tag;
+      tagName.href = "https://stackoverflow.com/tags/" + tag;
+      pageVisits.textContent = topTags[tag]["pages_visited"];
       slide.classList.add("carousel-item", "text-center", "p-4");
       if(tag === tags[0]) {
         slide.classList.add("active");
@@ -70,8 +56,6 @@ function loadCarosel() {
       innerBlock.appendChild(pageVisits);
       slide.append(innerBlock);
       carosel.append(slide);
-    //var info =  result["activity"].weekly_summary;
-    //console.log(info);
     }
   });
   var test = {
@@ -109,9 +93,9 @@ function loadCarosel() {
   function trackClick(element) {
     //Tracks whether one of the elements of interest has been clicked on.
     element.addEventListener('click', function (e) {
-      console.log('hello');
       chrome.runtime.sendMessage({type: "stackoverflow:click", url: element.href}, function(response) {
         console.log(response.testType + " " + response.testURL);
       });
+      chrome.tabs.create({url: element.href, active: false});
     });
   }
